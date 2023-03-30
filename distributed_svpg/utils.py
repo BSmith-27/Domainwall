2f022d69f68122826fc30f287bec9b6c2fb3bfa9
Rama Vasudevan <vasudevanrk@ornl.gov>
2023-03-30 13:36:19 -0400

    collapsed arrays edit

--- distributed_svpg/utils.py
+++ distributed_svpg/utils.py
@@ -148,16 +148,17 @@ def return_collapsed_array(sim_results, key):
     """given the list of simulation results, return the collapsed array
     so that all the batches are concatenated, as a list of tf tensors"""
 
-    print('Length of state history in return_collapsed_arr is {}'.format(len(sim_results)))
+    #print('Length of state history in return_collapsed_arr is {}'.format(len(sim_results)))
     state_history = [sim_results[ind][key] for ind in range(len(sim_results))]
-    state_history_shapes = [np.array(state).shape for state in state_history]
-    print(state_history_shapes)
-    state_history = np.array(state_history)
+    state_history_arrs = [np.array(state) for state in state_history]
+    state_history = np.vstack(state_history_arrs)
 
     if key =='state_history':
+        print("state history shape is {}".format(state_history.shape))
         state_history_full = state_history.reshape(-1, state_history.shape[-1])  # make it
         state_history_tf = tf.squeeze(tf.stack(state_history_full[:]))
     else:
+        print("other history shape is {}".format(state_history.shape))
         state_history_full = state_history.flatten()
         state_history_tf = tf.squeeze(tf.stack(state_history_full[:]))
 
