43bf9e063d0457c596f09ce50fb837af8f821f48
BSmith-27 <124918528+BSmith-27@users.noreply.github.com>
2023-07-06 12:47:45 -0400

    Update svpg.py

--- Supplementary/RL Code/distributed_svpg/svpg.py
+++ Supplementary/RL Code/distributed_svpg/svpg.py
@@ -49,7 +49,6 @@ def getLocalPolicyGrad(sim_results, verbose = False, svpg = False, gamma=0.99):
     reward_history_tf = return_collapsed_array(sim_results, 'reward_history')
     done_history_tf = return_collapsed_array(sim_results, 'done_history')
 
-    #print(state_history_tf.shape, actions_history_tf.shape,
     #      reward_history_tf.shape, done_history_tf.shape)
     state_history_tf = state_history_tf[:,:,None]
     _, old_state_values = policy(state_history_tf)
@@ -60,7 +59,6 @@ def getLocalPolicyGrad(sim_results, verbose = False, svpg = False, gamma=0.99):
     reward=reward_history_tf, done=done_history_tf)
     
     #Code below is sometimes useful for normalizing
-    #advantage_estimate = (advantage_estimate - tf.math.reduce_mean(advantage_estimate)) / tf.math.maximum(tf.math.reduce_std(advantage_estimate), tf.constant(1.0E-6))
 
     #Policy gradient
     dim_actions = policy.dim_actions
@@ -68,8 +66,7 @@ def getLocalPolicyGrad(sim_results, verbose = False, svpg = False, gamma=0.99):
         # Calculate the policy gradient
         actions_mean_tt = tf.reshape(policy.actor(state_history_tf), (-1, dim_actions, 2))
 
-        #print('actions mean shape is {}, actions history is {}'.format(actions_mean_tt.shape,
-        #                                                               actions_history_tf.shape))
+
 
         lognorm_dist = tfp.distributions.MultivariateNormalDiag(
             tf.nn.softplus(tf.reshape(actions_mean_tt, (done_history_tf.shape[0], dim_actions, 2))[:, :, 0]),
@@ -108,7 +105,6 @@ def run_simulation(numSimRuns = 10, agent_comms=[0,0], verbose = False):
     done_history = []
     episode_history = []
     px,py = agent_comms
-    #gym.make('BipedalWalker-v3')
 
     for _ in range(numSimRuns):
         state,_ = INS_Env.reset()  
@@ -118,9 +114,6 @@ def run_simulation(numSimRuns = 10, agent_comms=[0,0], verbose = False):
         if verbose: 
             print_rank('state shape is {}'.format(state.shape))
         while not done:
-            #print('epiode #{} with step #{}'.format(ep_num, steps))
-            # Take an action
-            #print('state is {}'.format(state))
 
             actions_output, _ = policy(state[None,:, None])
 
@@ -131,7 +124,6 @@ def run_simulation(numSimRuns = 10, agent_comms=[0,0], verbose = False):
                 tf.nn.softplus(actions_output[:, 0]), tf.nn.softplus(actions_output[:, 1])).sample(1)
 
             output_actions = np.squeeze(tf.clip_by_value(output_action, 0.1, 0.9).numpy())
-            #output_actions[1] = (output_actions[1]*2)-1 #voltage distribution shift
             if verbose:
                 print_rank("output actions is {}".format(output_actions))
                 print_rank("step number of episode is {}".format(INS_Env.step_num))
@@ -228,7 +220,6 @@ def train_svpg_MPI(iterations=800, batch_size=1, numSimRuns = 1, svpg = True,
             # Critic loss is calculated as well and applied, regardless of svpg status.
 
             localGrad = getLocalPolicyGrad(sim_results, svpg = svpg, gamma=gamma)
-            #print('localgrad is {}'.format(localGrad))
             #If doing SVPG
             if svpg:
                 # Gather them all up - all gradients and all weights for SVPG step
@@ -263,19 +254,6 @@ def train_svpg_MPI(iterations=800, batch_size=1, numSimRuns = 1, svpg = True,
             print_rank('Completed iteration {} with mean reward {:.4f} and max {:.4f}'.format(iteration,
                                                       np.mean(all_rewards),np.max(all_rewards)))
             
-            #print_rank('Completed iteration {}, agent rewards were {}'.format(iteration, all_rewards))
-
-        # for saving, need to only save the roots
-        '''
-        if iteration%10==0 and py==0 :
-            filepath_actor = 'Actor_' +str(px) + '_ep_' + str(iteration) + '.h5'
-            filepath_critic = 'Critic_' + str(px) + '_ep_' + str(iteration) + '.h5'
-
-            #policy.actor.save_weights(path.join(save_folder_name, filepath_actor))
-            #policy.critic.save_weights(path.join(save_folder_name, filepath_critic))
-            if px==0:
-                np.save(path.join(save_folder_name, filename_rewards), np.array(rewards_list))
-        '''
 
     t1 = time.time()
     print_rank('It took {:.2f}s to complete all iterations'.format(t1-t0))
