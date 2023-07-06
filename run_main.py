3c8ad788af6b7f080044a9c19a4d9db16c663833
BSmith-27 <124918528+BSmith-27@users.noreply.github.com>
2023-07-06 09:05:51 -0400

    Update run_main.py
    Updated hyperparameters

--- run_main.py
+++ run_main.py
@@ -28,6 +28,6 @@ if not os.path.exists(save_folder_name):
     except: 
         pass
     
-distributed_svpg.train_svpg_MPI(iterations = 1000, batch_size = 2, numSimRuns=4, svpg = False,
-                                gamma = 0.90, stein_temp = 10.0, save_folder_name = save_folder_name)
+distributed_svpg.train_svpg_MPI(iterations = 2000, batch_size = 2, numSimRuns=4, svpg = True,
+                                gamma = 0.99, stein_temp = 2.0, save_folder_name = save_folder_name)
 
