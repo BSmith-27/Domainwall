c251088c9483a048639773ba30b7e1578b6a2241
Rama Vasudevan <vasudevanrk@ornl.gov>
2024-01-02 17:32:00 -0500

    new weights

--- distributed_svpg/svpg.py
+++ distributed_svpg/svpg.py
@@ -30,7 +30,7 @@ tf.config.experimental.set_memory_growth(gpu_devices[world_rank // num_agents_pe
 #The policy is a global variable. There will be one policy per MPI process
 
 policy = NeuralNet_A2C(input_dim=(128,1), dim_actions=3, actor_lr = 0.001, critic_lr = 0.0013)
-model_name = r'trained_surrogate_weights.h5'
+model_name = r'trained_surrogate_new_weights.h5'
 model_path = os.path.join(os.path.abspath(os.getcwd()), 'distributed_svpg/')
 model_file = os.path.join(model_path,model_name)
 INS_Env = WallEnv(model_file = model_file)
