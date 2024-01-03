24ca34a66ac8708f7aa2ac93e2aee9a80c6d59f2
Rama Vasudevan <vasudevanrk@ornl.gov>
2024-01-03 13:59:48 -0500

    no gpu

--- distributed_svpg/svpg.py
+++ distributed_svpg/svpg.py
@@ -23,9 +23,9 @@ world_size = comm_world.Get_size()
 print("world rank is {} and world size is {}".format(world_rank, world_size))
 
 num_agents_per_gpu = 6
-gpu_devices = tf.config.experimental.list_physical_devices('GPU')
-tf.config.experimental.set_visible_devices(gpu_devices[world_rank // num_agents_per_gpu], 'GPU')
-tf.config.experimental.set_memory_growth(gpu_devices[world_rank // num_agents_per_gpu], True)
+#gpu_devices = tf.config.experimental.list_physical_devices('GPU')
+#tf.config.experimental.set_visible_devices(gpu_devices[world_rank // num_agents_per_gpu], 'GPU')
+#tf.config.experimental.set_memory_growth(gpu_devices[world_rank // num_agents_per_gpu], True)
 
 #The policy is a global variable. There will be one policy per MPI process
 
